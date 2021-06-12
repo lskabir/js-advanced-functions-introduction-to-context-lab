bcf69e4b0e4d8f2d070281cbd514d87c78ef355f
github username <github email address>
2021-06-11 21:24:41 -0400

    commit tree 26cd3d1f320c20b700689837ca65cb4868a4c9e2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecords) {
+    return employeeRecords.map(e => {
+        return createEmployeeRecord(e)
+    })
+}
+
+function createTimeInEvent(employee, dateTime) {
+    let dateTimeArray = dateTime.split(' ')
+    employee.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(dateTimeArray[1]),
+        date: dateTimeArray[0]
+    })
+    return employee
+}
+
+function createTimeOutEvent(employee, dateTime) {
+    let dateTimeArray = dateTime.split(' ')
+    employee.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateTimeArray[1]),
+        date: dateTimeArray[0]
+    })
+    return employee
+}
+
+function hoursWorkedOnDate(employee, date) {
+
+}
